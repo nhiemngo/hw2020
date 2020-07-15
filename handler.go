fb0c097c827e829e404f55e926fd82822b9566b3
Aditi Mittal <aditimittal@squareup.com>
2020-07-15 16:46:06 -0700

    added html/css for display page

--- handler.go
+++ handler.go
@@ -18,7 +18,7 @@ func functionHandler(w http.ResponseWriter, r *http.Request) {
 func viewTestSellerHandler(w http.ResponseWriter, r *http.Request) {
 	s := testSeller()
 
-	tmpl, err := template.ParseFiles("display.html")
+	tmpl, err := template.ParseFiles("templates/display.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -47,7 +47,7 @@ func (app *application) viewHandler(w http.ResponseWriter, r *http.Request) {
 		Lng:           lng,
 	}
 
-	tmpl, err := template.ParseFiles("display.html")
+	tmpl, err := template.ParseFiles("templates/display.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
